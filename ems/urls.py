a52578fef61befc40beed7d4c75f53da21d11b0d
Suhrid Mathur <suhrid.mathur@industrybuying.com>
2020-06-29 18:19:50 +0530

    Swagger API Url Change

--- ems/urls.py
+++ ems/urls.py
@@ -36,6 +36,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='swagger_schema_api'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger_schema_api'),
     path('', include(entity_urls)),
 ]
